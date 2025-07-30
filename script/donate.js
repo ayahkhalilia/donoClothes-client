5604a15f9e4251e72d91a93f6e3e539c1a06d064
Aseel Abu saleh <165641652+aseelas21@users.noreply.github.com>
2025-07-30 22:08:41 +0300

    created js to the button

--- script/donate.js
+++ script/donate.js
@@ -4,3 +4,14 @@
     const params = new URLSearchParams(window.location.search);
     const requestType = params.get('request');
 
+   document.addEventListener('DOMContentLoaded', () => {
+  const addRequestBtn = document.querySelector('.floating-btn');
+  if (!addRequestBtn) return;
+
+  addRequestBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    window.location.href = 'donationform.html';
+  });
+});
+ 
